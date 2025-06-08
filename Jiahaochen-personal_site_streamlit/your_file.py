bd88be69ac9d8a6b7182c6fdc5fb46edd4936ef8
jjjjjjhc <jiahaochen2000m@163.com>
2025-06-08 16:32:28 +0800

    Add files via upload

--- Jiahaochen-personal_site_streamlit/your_file.py
+++ Jiahaochen-personal_site_streamlit/your_file.py
@@ -6,7 +6,8 @@ def local_css(file_name):
     with open(file_name) as f: 
         st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True) 
 
-# 使用绝对路径加载 CSS 文件
-base_dir = os.path.dirname(os.path.abspath(__file__))
-css_path = os.path.join(base_dir, "static", "css", "style.css")
-local_css(css_path)
+# 输出当前工作目录
+st.write("cwd:", os.getcwd()) 
+
+# 使用当前工作目录构建 CSS 文件路径并加载 CSS
+local_css(os.path.join(os.getcwd(), "static", "css", "style.css"))
